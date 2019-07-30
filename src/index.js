5d10725ffe60151deeb30da77eb76c52ca3c6cb9
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-07-30 13:26:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-window.onload=function(){const navbarToggler=document.querySelector('.navbar-toggle');const menuCollapse=document.querySelector('.navbar-collapse');navbarToggler.addEventListener('click',function(){if(menuCollapse.classList.contains('in')){menuCollapse.classList.remove('in');navbarToggler.setAttribute('aria-expanded','false');navbarToggler.classList.add('collapsed')}
-else{menuCollapse.classList.add('in');navbarToggler.setAttribute('aria-expanded','true');navbarToggler.classList.remove('collapsed')}},{passive:!0});
-
 import "./css/main.css";
