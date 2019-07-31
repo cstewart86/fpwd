bd6a63b93f34833bf585184d3d5c055b398df570
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-07-31 19:54:56 +0100

    Update lazyload.js

--- site/static/js/lazyload.js
+++ site/static/js/lazyload.js
@@ -1,6 +1,8 @@
 function lazyload() {
 // Get all of the images that are marked up to lazy load
 const images = document.querySelectorAll('.lazy');
+
+if ("IntersectionObserver" in window) {
 const config = {
   // If the image gets within 50px in the Y axis, start the download.
   rootMargin: '50px 0px',
@@ -24,6 +26,11 @@ function onIntersection(entries) {
       preloadImage(entry.target);
     }
   });
+}
+} else {
+function isInViewport(elem){let bounding=elem.getBoundingClientRect();return(bounding.top>=0&&bounding.left>=0&&bounding.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&bounding.right<=(window.innerWidth||document.documentElement.clientWidth))}
+window.addEventListener('scroll',function(event){for(let i=0;i<images.length;i++){if(isInViewport(images[i])){if(images[i].getAttribute('data-src')){images[i].setAttribute('src',[i].getAttribute('data-src'));images[i].removeAttribute('data-src');if(images[i].getAttribute('data-srcset')){images[i].setAttribute('srcset',images[i].getAttribute('data-srcset'));images[i].removeAttribute('data-srcset')}
+images[i].classList.remove('lazy')}}}},!1)
 }
 }
 window.addEventListener('load',lazyload,false);
