7e226eb28b9843ad094264e3ade27123100c36d9
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-07-31 20:46:23 +0100

    Update lazyload.js

--- site/static/js/lazyload.js
+++ site/static/js/lazyload.js
@@ -1,8 +1,33 @@
-function lazyload() {
-// Get all of the images that are marked up to lazy load
-const images = document.querySelectorAll('.lazy');
+const defaults = {
+  imageLoadedClass: 'js-lazy-image--handled',
+  imageSelector: '.js-lazy-image',
+  // If the image gets within 50px in the Y axis, start the download.
+  rootMargin: '50px 0px',
+  threshold: 0.01
+};
 
-if ("IntersectionObserver" in window) {
+let config,
+    images,
+    imageCount,
+    observer;
+
+/**
+ * Fetches the image for the given URL
+ * @param {string} url
+ */
+function fetchImage(url) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+}
+
+/**
+ * Preloads the image
+ * @param {object} image
+ */
 function preloadImage(image) {
   const src = image.dataset.src;
   if (!src) {
@@ -11,34 +36,92 @@ function preloadImage(image) {
 
   return fetchImage(src).then(() => { applyImage(image, src); });
 }
-const config = {
-  // If the image gets within 50px in the Y axis, start the download.
-  rootMargin: '50px 0px',
-  threshold: 0.01
-};
 
-// The observer for the images on the page
-let observer = new IntersectionObserver(onIntersection, config);
-  images.forEach(image => {
-    observer.observe(image);
-  });
+/**
+ * Load all of the images immediately
+ * @param {NodeListOf<Element>} images
+ */
+function loadImagesImmediately(images) {
+  // foreach() is not supported in IE
+  for (let i = 0; i < images.length; i++) {
+    let image = images[i];
+    preloadImage(image);
+  }
+}
 
+/**
+ * Disconnect the observer
+ */
+function disconnect() {
+  if (!observer) {
+    return;
+  }
+
+  observer.disconnect();
+}
+
+/**
+ * On intersection
+ * @param {array} entries
+ */
 function onIntersection(entries) {
+  // Disconnect if we've already loaded all of the images
+  if (imageCount === 0) {
+    disconnect();
+    return;
+  }
+
   // Loop through the entries
-  entries.forEach(entry => {
+  for (let i = 0; i < entries.length; i++) {
+    let entry = entries[i];
     // Are we in viewport?
     if (entry.intersectionRatio > 0) {
+      imageCount--;
 
       // Stop watching and load the image
       observer.unobserve(entry.target);
       preloadImage(entry.target);
     }
-  });
+  }
 }
-} else {
-function isInViewport(elem){let bounding=elem.getBoundingClientRect();return(bounding.top>=0&&bounding.left>=0&&bounding.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&bounding.right<=(window.innerWidth||document.documentElement.clientWidth))}
-window.addEventListener('scroll',function(event){for(let i=0;i<images.length;i++){if(isInViewport(images[i])){if(images[i].getAttribute('data-src')){images[i].setAttribute('src',[i].getAttribute('data-src'));images[i].removeAttribute('data-src');if(images[i].getAttribute('data-srcset')){images[i].setAttribute('srcset',images[i].getAttribute('data-srcset'));images[i].removeAttribute('data-srcset')}
-images[i].classList.remove('lazy')}}}},!1)
+
+/**
+ * Apply the image
+ * @param {object} img
+ * @param {string} src
+ */
+function applyImage(img, src) {
+  // Prevent this from being lazy loaded a second time.
+  img.classList.add(config.imageLoadedClass);
+  img.src = src;
 }
-}
-window.addEventListener('load',lazyload,false);
+
+let LazyLoad = {
+
+  init: (options) => {
+    config = {...defaults, ...options};
+
+    images = document.querySelectorAll(config.imageSelector);
+    imageCount = images.length;
+
+    // If we don't have support for intersection observer, loads the images immediately
+    if (!('IntersectionObserver' in window)) {
+      loadImagesImmediately(images);
+    } else {
+      // It is supported, load the images
+      observer = new IntersectionObserver(onIntersection, config);
+
+      // foreach() is not supported in IE
+      for (let i = 0; i < images.length; i++) {
+        let image = images[i];
+        if (image.classList.contains(config.imageLoadedClass)) {
+          continue;
+        }
+
+        observer.observe(image);
+      }
+    }
+  }
+};
+
+export default LazyLoad;
