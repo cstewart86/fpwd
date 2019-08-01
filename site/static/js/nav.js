b0cc1b97b712bb66ec8ea38ca7ea39476df1b953
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-08-01 12:40:09 +0100

    Update nav.js

--- site/static/js/nav.js
+++ site/static/js/nav.js
@@ -1,3 +1,2 @@
-function toggleNav(){const navbarToggler=document.querySelector('.navbar-toggle');const menuCollapse=document.querySelector('.navbar-collapse');navbarToggler.addEventListener('click',function(){if(menuCollapse.classList.contains('in')){menuCollapse.classList.remove('in');navbarToggler.setAttribute('aria-expanded','false');navbarToggler.classList.add('collapsed')}
+window.onload = function(){const navbarToggler=document.querySelector('.navbar-toggle');const menuCollapse=document.querySelector('.navbar-collapse');navbarToggler.addEventListener('click',function(){if(menuCollapse.classList.contains('in')){menuCollapse.classList.remove('in');navbarToggler.setAttribute('aria-expanded','false');navbarToggler.classList.add('collapsed')}
 else{menuCollapse.classList.add('in');navbarToggler.setAttribute('aria-expanded','true');navbarToggler.classList.remove('collapsed')}},{passive:!0});
-toggleNav();
