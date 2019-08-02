ac2d4e2097d6cab7210963aa5eb3ba664ed48eed
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-08-02 20:37:27 +0100

    Update share.js

--- site/static/js/share.js
+++ site/static/js/share.js
@@ -3,7 +3,6 @@ const canonicalElement = document.querySelector('link[rel=canonical]');
 if (canonicalElement !== null) {
     url = canonicalElement.href;
 }
-navigator.share({url: url});
 
 if (navigator.share) {
   navigator.share({
