096d2ebf1f01b26113049267c7bb2644f1b60764
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-08-13 17:11:56 +0100

    Update sw.js

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const version = '05082019::';
+const version = '13082019::';
 // Caches for different resources
 const coreCacheName = version + 'core';
 const pagesCacheName = version + 'pages';
